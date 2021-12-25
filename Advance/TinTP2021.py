f01ebfb129e8e6984e84ca9021b04027e1da1411
Trung Hoang <trung.nang.hoang@gmail.com>
2021-12-25 01:06:43 -0500

    Added more problems

--- Advance/TinTP2021.py
+++ Advance/TinTP2021.py
@@ -62,3 +62,48 @@ def prob4(N, M, K, graph, targets):
 
 print(prob4(6, 2, 2, [[1, 2], [3, 2], [3, 5], [4, 2], [5, 6]], [1, 3]))
 print(prob4(6, 3, 2, [[1, 2], [3, 2], [3, 5], [4, 1], [5, 6]], [1, 3, 5]))
+
+# %%
+
+
+class Prob5:
+    def prob5(self, N, T, to_store1, to_store2, from_store1, from_store2):
+
+        mem = {}
+        visited = [0] * T
+        self.ans = float("inf")
+
+        def dfs(shipper=0, total=0, deliveries=0):
+            if deliveries == T:
+                self.ans = min(self.ans, total)
+                return
+            if total >= self.ans:
+                return
+
+            for cust in range(T):
+                if visited[cust]:
+                    continue
+                for dist in options[(shipper, cust)]:
+                    if dist:
+                        visited[cust] = ~shipper
+                        dfs(shipper + 1, total + dist, deliveries + 1)
+                    else:
+                        dfs(shipper + 1, total + dist, deliveries)
+                    visited[cust] = 0
+
+        options = collections.defaultdict(list)
+        for n in range(N):
+            for t in range(T):
+                options[(n, t)].append(to_store1[n] + from_store1[t])
+                options[(n, t)].append(to_store2[n] + from_store2[t])
+                options[(n, t)].append(0)
+
+        dfs()
+        return self.ans
+
+
+print(Prob5().prob5(3, 2, [1, 2, 2], [7, 5, 3], [3, 5], [2, 3]))
+
+N = 5
+T = 4
+print(Prob5().prob5(N, T, [2]*N, [3]*N, [2]*T, [3]*T))
