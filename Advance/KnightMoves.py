cce31a4d3f1bfc4d88c9f59735aa829b35af16dc
Trung Hoang <trung.nang.hoang@gmail.com>
2022-01-24 14:21:22 -0500

    Added followup

--- Advance/KnightMoves.py
+++ Advance/KnightMoves.py
@@ -29,5 +29,38 @@ def main(pos="e4", moves="AGG"):
     return chr(x + 65) + str(y)
 
 
-print(main("e4", "AGG"))  # h8
-print(main("e4", "AHCDEFACD"))  # a1
+def q1(pos=(5, 4), moves="AGG"):
+    """
+    We want to see the candidate utilizes dictionary
+    """
+    delta = {"A": (-1, 2), "B": (-2, 1), "C": (-2, -1), "D": (-1, -2),
+             "E": (1, -2), "F": (2, -1), "G": (2, 1), "H": (1, 2)}
+    x, y = pos
+    for move in moves:
+        dx, dy = delta[move]
+        x += dx
+        y += dy
+    return x, y
+
+
+def q2(pos=(5, 4), moves="AGG"):
+    """
+    We want to see the candidate uses BFS or DFS
+    """
+    delta = {"A": (-1, 2), "B": (-2, 1), "C": (-2, -1), "D": (-1, -2),
+             "E": (1, -2), "F": (2, -1), "G": (2, 1), "H": (1, 2)}
+    x0, y0 = pos
+    x1, y1 = q1(pos, moves)
+    queue = [[x0, y0, 0]]
+    for x, y, depth in queue:
+        if x == x1 and y == y1:
+            return depth == len(moves)
+        for dx, dy in delta.values():
+            queue.append([x + dx, y + dy, depth + 1])
+    return False
+
+
+# print(main("e4", "AGG"))  # h8
+# print(main("e4", "AHCDEFACD"))  # a1
+print(q2((5, 4), "AGG"))  # h8
+print(q2((5, 4), "AHCDEFACD"))  # a1
