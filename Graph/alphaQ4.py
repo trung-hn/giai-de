03be1ceeedff9d74fa18cf7e65e629c7bbbe1d74
Trung Hoang <trung.nang.hoang@gmail.com>
2022-06-05 12:32:20 -0400

    Added bfs q

--- Graph/alphaQ4.py
+++ Graph/alphaQ4.py
@@ -39,3 +39,29 @@ for destination in nodes:
     rv = get_shortest_path_to_source(graph, destination, sources)
     total += rv
 print(total)
+
+# %%
+
+
+def bfs(r, c):
+    visited = [[0] * M for _ in range(N)]
+    queue = [(r, c)]
+    for r, c in queue:
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            x, y = r + dx, c + dy
+            if 0 <= x < M and 0 <= y < N and visited[x][y] == 0:
+                visited[x][y] = 1
+                queue.append((x, y))
+        all_paths.add((tuple(row) for row in visited))
+
+
+M = 2
+N = 2
+
+starts = range(M)
+
+all_paths = set()
+for start in starts:
+    bfs(start, 0)
+
+print(len(all_paths))
